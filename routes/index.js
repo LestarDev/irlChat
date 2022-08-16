1a1e6466c6027144c25dd9c0f239bf987c8f1a91
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-08-16 22:17:53 +0100

    npm i

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const s = require('socket.io');
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = process.env.PORT || 3700;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var midPort = app.listen(port, function () {
+  console.log('Node.js listening on port ' + port);
+})
+
+const httpServer = createServer();
+const io = new Server(httpServer, {});
+io.on("connection", (socket) => {
+  sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
+    socket.on('send', function (data) {
+      io.sockets.emit('message', data);
+  })});
+});
+
+httpServer.listen(3000);
 
 module.exports = router;
