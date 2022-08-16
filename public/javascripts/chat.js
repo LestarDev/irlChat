6e2bcc3542bd87ba6fd22fa9d10a656e76ab4fbf
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-08-16 21:49:56 +0100

    add vars

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-    
+
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var name = document.getElementById("name");
+
 }
